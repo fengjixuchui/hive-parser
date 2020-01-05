cf7200638c5d01491da446f1e56971d4907c0c30
kyledotterrer <kdotterrer@gmail.com>
2020-01-04 23:07:31 -0500

    initial commit

---
+++ ConsoleClient/ConsoleClient.cs
@@ -0,0 +1,34 @@
+﻿// ConsoleClient.cs
+// Simple console client application for registry hive parsing library.
+
+using System;
+using HiveParserLib;
+
+namespace ConsoleClient
+{
+    class ConsoleClient
+    {
+        static void Main(string[] args)
+        {
+            RegistryHive hive = new RegistryHive(args[0]);
+            Console.WriteLine("The root key's name is: " + hive.RootKey.Name);
+
+            Byte[] bootkey = null;
+
+            try
+            {
+                bootkey = Utilities.GetBootKey(hive);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception thrown by GetBootKey():");
+                Console.WriteLine(e.Message);
+            }
+
+            if (bootkey != null)
+            {
+                Console.WriteLine("Boot key: " + BitConverter.ToString(bootkey));
+            }
+        }
+    }
+}
